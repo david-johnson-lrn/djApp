4f60db15256951c66106934350f24e933d9fc40e
DJ <dj@david-johnson.local>
2021-08-02 16:28:09 -0400

    adding SDK

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- Need to Do
+1. Call DataAPI to pull all djApp tags 
+2. Add Bootstrap to style
+-->
+
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="/authorAPI.php">
+        <button> Author API </button>
+    </a>
+</body>
+
+</html>
